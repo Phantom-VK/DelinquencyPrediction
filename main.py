f58cdd9a029d8a9954c6f66a079cd01861ae8ae0
Phantom-VK <vikramadityakhupse@gmail.com>
2025-09-01 13:22:28 +0530

    Added model fine tuning

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from evaluation import evaluate_model
-from model import create_model, save_model
-import pandas as pd
+from scripts.evaluation import evaluate_model
+from scripts.model import create_model, save_model
 
-from training import train_model
+from scripts.tune import tune_hyperparameters
 
 if __name__ == '__main__':
     model = create_model()
     print("Model Created")
 
-    data_path = "Cleaned_delinquency_prediction_dataset.csv"
-    trained_model, X_test, y_test = train_model(data_path)
+    data_path = "Resources/Cleaned_delinquency_prediction_dataset.csv"
+    # trained_model, X_test, y_test = train_model(data_path)
+    best_model, X_test, y_test = tune_hyperparameters(data_path)
+    save_model(best_model)
 
-    save_model(trained_model)
-
-    metrics = evaluate_model(trained_model, X_test, y_test)
+    metrics = evaluate_model(best_model, X_test, y_test)
 
     print(metrics)
---
+++ scripts/tune.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+
+
+
+def tune_hyperparameters(data_path):
+    """
+    Tune model hyperparameters
+    :param data_path: Path to dataset
+    :return: best estimator, X_test, y_test
+    """
+    df = pd.read_csv(data_path)
+
+    X = df.drop(columns=['Delinquent_Account'])
+    y = df['Delinquent_Account']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+
+    model = DecisionTreeClassifier(random_state=42)
+
+    param_grid = {
+        'criterion': ['gini', 'entropy'],
+        'max_depth': [None, 5, 10, 15, 20, 30],
+        'min_samples_split': [2, 5, 10, 20],
+        'min_samples_leaf': [1, 2, 5, 10],
+        'max_features': [None, 'sqrt', 'log2', 0.8, 1.0],
+        'splitter': ['best', 'random']
+    }
+
+    grid_search = GridSearchCV(
+        estimator=model,
+        param_grid=param_grid,
+        cv = 5,
+        scoring='f1',
+        n_jobs=-1,
+        verbose=2
+    )
+
+    grid_search.fit(X_train, y_train)
+    print("Best score: {:.3f}".format(grid_search.best_score_))
+    print("Best params: {}".format(grid_search.best_params_))
+    return grid_search.best_estimator_, X_test, y_test
